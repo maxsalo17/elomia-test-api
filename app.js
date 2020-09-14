2e5b56f20a9c9c8f5e5a24f5fe9b6203c5822154
maxsalo17 <makssalo13@gmail.com>
2020-09-14 17:19:42 +0300

    main test api functionallity

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bluebird = require('bluebird')
+const bodyParser = require('body-parser')
+const config = require('./config.json')
 const app = express()
 const port = process.env.PORT || 3000
+const apiRoutes = require('./lib/routes')
 
-app.get('/', (req, res) => {
-  res.send('Test')
-})
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+const startServer = () => {
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use('/api', apiRoutes)
+    app.listen(port)
+    console.log(`App started on port ${port}`)
+}
+
+const connectDb = () => {
+    mongoose.Promise = bluebird
+
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+    mongoose.connect(config.mongodb, options)
+    return mongoose.connection
+}
+
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('open', startServer)
